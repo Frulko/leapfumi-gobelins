0fdcbffbecdd18ca950cec5133eeae6525ab8b5e
guillaume <gdumoulin@me.com>
2013-03-15 11:36:46 +0100

    Update MIME

--- app.js
+++ app.js
@@ -1,9 +1,47 @@
-var html = require('fs').readFileSync(__dirname+'/index.html');
-var server = require('http').createServer(function(req, res){
-    res.end(html);
+var fs = require('fs');
+var path = require('path');
+var app = require('http').createServer(function(req, res){
+
+    var filePath = req.url;
+    var test = filePath.substring(2);
+    var regex = /id/;
+
+    if (filePath == '/' || test.match(regex)) {
+        filePath = '/index.html';
+    }
+    else{
+        var patt1 = /\.([0-9a-z]+)/i;
+        var m5 = filePath.match(patt1);
+    }
+
+    var extname = path.extname(filePath); //Gestion des extensions de fichier et changement du type MIME
+    var contentTypesByExtention = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css':  'text/css'
+    };
+
+    var contentType = contentTypesByExtention[extname] || 'text/plain';
+    fs.exists(__dirname+filePath, function(exists) {
+
+        if (exists) {
+            var html = require('fs').readFileSync(__dirname+filePath);
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(html, 'utf-8');
+        }
+        else {
+            res.writeHead(404);
+            res.end("Erreur 404");
+        }
+    });
+
+
 });
-server.listen(1337);
+app.listen(1337);
+
 console.log("Serveur NodeJS for LEAP MOTION");
+
+
 var nowjs = require("now");
 var everyone = nowjs.initialize(server);
 var clients = {}; // Attention ne pas initialiser avec [] car bug lors de la transmission de la variable au client
